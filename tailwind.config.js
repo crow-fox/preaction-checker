128c14b58723bdeff14f7b1693a4ee48cc4a6f72
crow-fox <surakamiya198@gmail.com>
2023-06-02 19:37:06 +0900

    初期化

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black: {
+          light: "#C8C8C8",
+          DEFAULT: "#222222",
+        },
+        gray: {
+          DEFAULT: "#F0F0F0",
+          2: "A0A0A0",
+          dark: "#505050",
+        },
+        red: {
+          DEFAULT: "#F0C8C8",
+          dark: "#502828",
+        },
+        green: {
+          DEFAULT: "#C8F0C8",
+          dark: "#285028",
+        },
+        blue: {
+          DEFAULT: "#C8F0F0",
+          dark: "#285050",
+        },
+        yellow: {
+          DEFAULT: "#F0F0C8",
+          dark: "#505028",
+        },
+        pink: {
+          DEFAULT: "#F0C8F0",
+          dark: "#502850",
+        },
+      },
+      maxWidth: {
+        container: "45rem",
       },
     },
   },
   plugins: [],
-}
+};
