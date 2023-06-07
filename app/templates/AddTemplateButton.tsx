f2265c5233348687183e1dbdb5ae58e9537c68d1
crow-fox <surakamiya198@gmail.com>
2023-06-07 11:34:34 +0900

    fix/addTemplateButton

--- app/templates/AddTemplateButton.tsx
+++ app/templates/AddTemplateButton.tsx
@@ -1,11 +1,25 @@
 "use client";
 
-import AddButton from "@/app/_components/AddButton";
+import AddIcon from "@/app/_components/icons/AddIcon";
 import TemplateIcon from "@/app/_components/icons/TemplateIcon";
 import { useTemplates } from "@/app/templates/useTemplates";
 
 export default function AddTemplateButton() {
   const { addTemplate } = useTemplates();
 
-  return <AddButton icon={<TemplateIcon />} onClick={addTemplate} />;
+  return (
+    <button
+      type="button"
+      onClick={addTemplate}
+      className="flex w-full items-center justify-between gap-4 rounded border-2 border-dotted border-slate-500 p-4 hover:border-solid hover:border-blue-700 hover:bg-white focus:border-solid focus:border-blue-700 focus:bg-white  "
+    >
+      <span className="flex items-center gap-2 text-2xl  leading-normal">
+        <TemplateIcon />
+        <span className="font-bold">新規追加</span>
+      </span>
+      <span className="ml-auto text-2xl">
+        <AddIcon />
+      </span>
+    </button>
+  );
 }
